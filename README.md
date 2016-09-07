844ba2874783d00cf55fc41e7a0ce59a0ac7628a
Dmitry <yankelevich.dima@gmail.com>
2016-09-07 22:23:38 +0300

    added readme and script

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 6_password_strength
+
+## Usage
+
+ - Run `python password_strength.py <password>`
+ - Note that first run require downloading ~20MB of data (blacklist of passwords)
+
+## Scores
+
+Script returns result score from 1 to 10 (the bigger, the better)
+
+  - \+ 1 point if password contains digits
+  - \+ 2 points if length more or equal 6
+  - \+ 2 points if length more or equal 10
+  - \+ 2 points if password contains special symbols
+  - \+ 2 points if password contains both uppercase and lowercase symbols
+
+Note that if password is contained in blacklist, script returns 1 point.  
+Default blacklist of password you can find [here](https://dazzlepod.com/site_media/txt/passwords.txt).
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,67 @@
+import re
+import os
+import sys
+import datetime
+import urllib.request
+from functools import reduce
 
+
+BLACKLIST_URL = 'https://dazzlepod.com/site_media/txt/passwords.txt'
+BLACKLIST_PATH = './blacklist.txt'
+
+
+def get_or_download_blacklist():
+    if not os.path.exists(BLACKLIST_PATH):
+        print('Downloading blacklist (~20MB) ...')
+        urllib.request.urlretrieve(BLACKLIST_URL, BLACKLIST_PATH)
+
+    with open(BLACKLIST_PATH, 'r') as infile:
+        data = infile.read()
+    words = data.split('\n')
+    # First 15 lines are comments in default list
+    return words[15:]
 
 def get_password_strength(password):
-    pass
+    scores = {}
+    weights = {
+        'length': 2,
+        'have_digits': 1,
+        'special_symbols': 2,
+        'letters_type': 2,
+    }
+
+    # Password length
+    if len(password) < 6:
+        scores['length'] = 0
+    if 5 < len(password) < 10:
+        scores['length'] = 1
+    elif len(password) > 9:
+        scores['length'] = 2
+
+    # Have digits
+    if re.search('[0-9]', password):
+        scores['have_digits'] = 1
+
+    # Have special symbols
+    if re.search('[\W_]+', password):
+        scores['special_symbols'] = 1
+
+    # Have both type of letters
+    if re.search('[a-zа-я]', password) and re.search('[A-ZА-Я]', password):
+        scores['letters_type'] = 1
+
+    # Exists in BLACKLIST (Very bad password - always return 1)
+    if password in get_or_download_blacklist():
+        return 1
+
+    total_score = reduce(
+        lambda res, key_value: res + weights[key_value[0]] * key_value[1],
+        scores.items(),
+        1
+    )
+
+    return total_score
 
 
 if __name__ == '__main__':
-    pass
+    print(get_password_strength(sys.argv[1]))
